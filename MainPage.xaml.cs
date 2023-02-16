2a1b1704bda07c9d134d2c737e2255a72d8aa9bd
User <User@LAB3-3>
2023-02-16 09:28:19 +0200

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace BMICalculator;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	public void btn_Clicked(object sender, EventArgs e)
+	{
+		Main_Lbl.Text = (Weight.Value / Math.Pow(HeightSlider.Value / 100, 2)).ToString();
+	}
+}
+
